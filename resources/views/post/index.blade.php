dc037e3b871679345441a6878216ce7560b482e4
gregory <graigtech@gmail.com>
2022-07-31 19:01:10 +0400

    arrangement récupération user

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -4,9 +4,9 @@
 
 
 <!-- page d'accueil  -->
-<div class="creer"><a href="{{ route('posts.create') }}">Créer un post</a></div>
+<!-- <div class="creer"><a href="{{ route('posts.create') }}">Créer un post</a></div>
 
-<div class="creer"><a href="{{ route('dashboard') }}">Dashboard</a></div>
+<div class="creer"><a href="{{ route('dashboard') }}">Dashboard</a></div> -->
 
 <div class="galerie">
 
@@ -18,26 +18,22 @@
   </div>
   @endif <div class="item">
 
-    <a href="{{ route('posts.show', $post)}}"><img src="{{ asset('storage/' . $post->image)}}" class="cardimg" alt="...">
-    </a>
+    <a href="{{ route('posts.show', $post) }}"><img src="{{ URL::asset('/image/k77.jpg') }}" class="cardimg"></a>
+    <!-- alt="{{URL::asset('/image/antique.jpg')}}" -->
 
     <div class="c-body">
       <h5 class="c-title">{{ $post->nom_objet}}</h5>
 
-      <a href="{{ route('posts.destroy', $post)}}">
-        Supprimmer
-      </a>
-
       <p class="c-text">
         {{ Str::limit($post->description, 50)}}
       </p>
+
       <p class="c-text">
-        {{ $post->user->name }}
+        Par {{ $post->user->name }} le {{ $post->created_at->format('d N Y')}}
       </p>
-      <p class="c-text">
-        {{ $post->created_at->format('d N Y')}}
-      </p>
+ 
       <a href="{{ route('posts.show', $post)}}" class="btn btn-primary">Voir</a>
+
       <a href="{{ route('posts.destroy', $post)}}">
         Supprimmer
       </a>
@@ -45,7 +41,6 @@
 
   </div>
   @endforeach
-  <!-- <input type="submit" value="Supprimer"> -->
 
 </div>
 @include('layouts.footer')
@@ -55,4 +50,5 @@
     mail : greg@mail.com
     password : 11111111 
     {{ Auth::user()->name }} ;
+    {{ asset('storage/' . $post->image)}}
     URL::asset('/image/k77.jpg')-->
